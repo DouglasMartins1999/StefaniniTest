64d1477380e374d35166f108154f1dcd3f8d3fcf
Douglas Martins <douglas.martins.m099@gmail.com>
2022-09-15 18:35:44 -0300

    [Feature] CheapShark API Integration

--- game-tracker/script.js
+++ game-tracker/script.js
@@ -1,80 +1,12 @@
 const jogosLista = document.getElementById("jogos-lista");
+const imageFallback = "assets/sem-imagem.jpg";
+const APIURL = "https://www.cheapshark.com/api/1.0/deals?pageSize=12&storeID=1&onSale=1&AAA=1"
 
-let ofertas = [
-  {
-    title: "RAGE 2",
-    salePrice: "0,00",
-    normalPrice: "199,00",
-    thumb: "assets/imgs/548570.jpg",
-  },
-  {
-    title: "Batman™: Arkham Knight",
-    salePrice: "12,49",
-    normalPrice: "49,99",
-    thumb: "assets/imgs/208650.jpg",
-  },
-  {
-    title: "The Sims™ 4",
-    salePrice: "39,75",
-    normalPrice: "159,00",
-    thumb: "assets/imgs/1222670.jpg",
-  },
-  {
-    title: "Street Fighter V",
-    salePrice: "15,99",
-    normalPrice: "39,99",
-    thumb: "assets/imgs/310950.jpg",
-  },
-  {
-    title: "Divinity: Original Sin 2 - Definitive Edition",
-    salePrice: "36,39",
-    normalPrice: "90,99",
-    thumb: "assets/imgs/435150.jpg",
-  },
-  {
-    title: "Planet Zoo",
-    salePrice: "50,00",
-    normalPrice: "100,00",
-    thumb: "assets/imgs/703080.jpg",
-  },
-  {
-    title: "Battlefield V",
-    salePrice: "119,60",
-    normalPrice: "299,00",
-    thumb: "assets/imgs/1238810.jpg",
-  },
-
-  {
-    title: "Arma 3",
-    salePrice: "17,49",
-    normalPrice: "69,99",
-    thumb: "assets/imgs/107410.jpg",
-  },
-  {
-    title: "Zombie Army 4: Dead War",
-    salePrice: "84,59",
-    normalPrice: "93,99",
-    thumb: "assets/imgs/694280.jpg",
-  },
-  {
-    title: "Sniper Ghost Warrior Contracts",
-    salePrice: "34,99",
-    normalPrice: "69,99",
-    thumb: "assets/imgs/973580.jpg",
-  },
-  {
-    title: "Jurassic World Evolution",
-    salePrice: "19,99",
-    normalPrice: "79,99",
-    thumb: "assets/imgs/648350.jpg",
-  },
-  {
-    title: "RollerCoaster Tycoon® 3: Complete Edition",
-    salePrice: "22,79",
-    normalPrice: "37,99",
-    thumb: "assets/imgs/1368820.jpg",
-  },
-];
+const opcoes = {
+  buscaAtiva: "",
+  ordAtual: "Deal Rating",
+  pagAtual: 0
+}
 
 function calcularDesconto(precoNormal, precoAtual) {
   precoNormal = parseFloat(precoNormal);
@@ -87,14 +19,63 @@ function calcularDesconto(precoNormal, precoAtual) {
   return `-${Math.round(porcentagem)}%`;
 }
 
-window.onload = () => {
+function carregarDados(url = APIURL) {
+  if(opcoes.pagAtual)
+    url += `&pageNumber=${opcoes.pagAtual}`;
+  
+  if(opcoes.buscaAtiva)
+    url += `&title=${opcoes.buscaAtiva}`;
+    
+  if(opcoes.ordAtual)
+    url += `&sortBy=${opcoes.ordAtual}`;
+
+  if(opcoes.ordDecres)
+    url += `&desc=1`;
+    
+  return fetch(url)
+    .then(resp => resp.json())
+    .then(data => renderizarOfertas(data))
+    .catch(() => alert("Erro ao Carregar as Ofertas"));
+}
+
+function buscarJogos(event) {
+  event.preventDefault();
+  opcoes.pagAtual = 0;
+  opcoes.buscaAtiva = event.target.value;
   jogosLista.innerHTML = "";
 
-  ofertas.map((oferta) => {
+  carregarDados()
+}
+
+function ordenarLista(event) {
+  const valores = event.target.value?.split(",");
+
+  event.preventDefault();
+  opcoes.pagAtual = 0;
+  opcoes.ordDecres = !!valores[1];
+  opcoes.ordAtual = valores[0];
+  jogosLista.innerHTML = "";
+
+  carregarDados()
+}
+
+function proximaPagina() {
+  opcoes.pagAtual++;
+
+  carregarDados()
+}
+
+function fallbackImagem(image, url = imageFallback) {
+  image.onError = null;
+  image.src = url;
+}
+
+function renderizarOfertas(data) {
+  data.map(oferta => {
     jogosLista.innerHTML += `
       <article class="oferta">
         <figure>
-          <img src="${oferta.thumb}" alt="${oferta.title}">
+          <img src="https://cdn.akamai.steamstatic.com/steam/apps/${oferta.steamAppID}/header.jpg" onerror="fallbackImagem(this)" alt="${oferta.title}">
         </figure>
 
         <section class="conteiner-jogo">
@@ -113,4 +94,8 @@ window.onload = () => {
       </article>
       `;
   });
+}
+
+window.onload = () => {
+  carregarDados()
 };
